f6a5f07c70b1bf39c437412512cb593e7b7c392f
Jared <jst9259@gmail.com>
2017-07-30 21:35:59 -0400

    mark 2

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+
+{
+  "name":"Lil Jon",
+  "photo":"https://qph.ec.quoracdn.net/main-qimg-96ae2535d27ff43798b407d90f8d66e3",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+
+{
+  "name":"Lil Wayne",
+  "photo":"http://media.tmz.com/2016/11/02/110216-lil-wayne-kal-v3-1080x608.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Bonecrusher",
+  "photo":"http://images2.mtv.com/uri/mgid:uma:artist:mtv.com:1235147?width=1200&height=900",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+
+]
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 3000;
+
+var jsonParser = bodyParser.json()
+
+var unlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+app.use(bodyParser.text({ type: 'text/html'}))
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
